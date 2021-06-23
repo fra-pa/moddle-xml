338ab91d71be5c429236986b5f11b86448fe3d2b
Frank Panno <git@fra-pa.de>
2021-06-23 22:21:12 +0200

    test(read): add spec to read properties from multiple namespaces

--- test/spec/multiple-inherited-properties.js
+++ test/spec/multiple-inherited-properties.js
@@ -1,6 +1,6 @@
 import expect from '../expect';
 import { createModelBuilder } from '../helper';
-import { Writer } from '../../lib';
+import { Reader, Writer } from '../../lib';
 import { assign } from 'min-dash';
 
 
@@ -8,23 +8,23 @@ describe('Multiple Inherited Properties', function() {
 
   var createModel = createModelBuilder('test/fixtures/model/');
 
+  var model = createModel([
+    'properties',
+    'multiple-inherited-properties'
+  ]);
+
   describe('Writer', function() {
 
     function createWriter(model, options) {
       return new Writer(assign({ preamble: false }, options || {}));
     }
 
-    var mhModel = createModel([
-      'properties',
-      'multiple-inherited-properties'
-    ]);
-
     it('should write properties from multiple namespaces', function() {
 
       // given
-      var writer = createWriter(mhModel);
+      var writer = createWriter(model);
 
-      var root = mhModel.create('mh:MultipleInherited', {
+      var root = model.create('mh:MultipleInherited', {
         'id': 'Root_1',
         'single': 'mh-single',
         'props:single': 42
@@ -45,4 +45,41 @@ describe('Multiple Inherited Properties', function() {
 
   }); // describe(multiple inherited properties/Writer)
 
+  describe('Reader', function() {
+
+    it('should read properties from multiple namespaces', async function() {
+
+      // given
+      var reader = new Reader(model);
+      var rootHandler = reader.handler('mh:MultipleInherited');
+
+      var xml =
+          '<mh:MultipleInherited ' +
+          'xmlns:mh="http://multipleinheritance" ' +
+          'xmlns:props="http://properties" ' +
+          'props:single="42" single="mh-single" />';
+
+      // when
+      var {
+        rootElement,
+        elementsById,
+        warnings,
+        references
+      } = await reader.fromXML(xml, rootHandler);
+
+      console.log(elementsById);
+      var expectedElement = {
+        '$type': 'mh:MultipleInherited',
+        'props:single': 42,
+        single: 'mh-single'
+      };
+
+      // then
+      expect(elementsById).to.eql({});
+      expect(rootElement).to.exist;
+      expect(rootElement).to.jsonEqual(expectedElement);
+      expect(warnings).to.eql([]);
+      expect(references).to.eql([]);
+    });
+  }); // describe(multiple inherited properties/Reader)
 }); // describe(multiple inherited properties)
